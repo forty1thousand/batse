4f06767fcec31ee537abe00aa413747ce2373ec3
Gavin Bates <chessinato@gmail.com>
2024-10-24 22:28:41 -0400

    Fix bug

--- app/lib/cookie.ts
+++ app/lib/cookie.ts
@@ -1,6 +1,6 @@
 import Cryptr from "cryptr";
 
-let cryptr = new Cryptr(process.env.SECRET!);
+let cryptr = new Cryptr(process.env.SECRET! ?? "HARDCODEDBISHKEKcarts");
 
 export let encrypt = cryptr.encrypt;
 export let decrypt = cryptr.decrypt;
