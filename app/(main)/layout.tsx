16749cae23151d2d6a56801d7b0f471cc9592adc
Gavin Bates <chessinato@gmail.com>
2024-11-07 22:39:59 -0500

    Fix

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { Container } from "@/app/components/container";
+import Leaf from "@/app/components/leaf";
 import { Line } from "@/app/components/line";
 
 export default function ({ children }: React.PropsWithChildren) {
@@ -9,6 +10,7 @@ export default function ({ children }: React.PropsWithChildren) {
         <div className="flex items-center">
           <p className="font-bold text-5xl leading-none">Batse</p>
           <Line className="flex-1 mx-4" />
+          <Leaf className="fill-destructive stroke-transparent size-6" />
         </div>
         <ul className="flex text-muted text-xs mt-4 [&>*:nth-last-child(n+2)]:after:content-['—'] *:after:mx-2 ">
           <li>
---
+++ app/sitemap.ts
@@ -0,0 +1,30 @@
+import { MetadataRoute } from "next";
+
+export default function (): MetadataRoute.Sitemap {
+  return [
+    {
+      url: process.env.NEXT_PUBLIC_BASE_URL!,
+      priority: 1,
+      changeFrequency: "yearly",
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL!}/signup`,
+      priority: 0.5,
+      changeFrequency: "yearly",
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL!}/login`,
+      priority: 0.5,
+      changeFrequency: "yearly",
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.NEXT_PUBLIC_BASE_URL!}/search`,
+      priority: 0.5,
+      changeFrequency: "yearly",
+      lastModified: new Date(),
+    },
+  ];
+}
